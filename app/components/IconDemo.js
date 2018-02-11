cb140eaf4b3b54cff9769cdc33641de53eae85d1
makachung0 <makachung0@gmail.com>
2018-02-11 21:22:08 +0800

    Add More Button
    1. Add IconDemo demo page for showing different icon layout
    2. Add More Button

---
+++ app/components/IconDemo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MoreButton from './buttons/MoreButton';
+
+class IconDemo extends Component {
+  render() {
+    return (
+      <MoreButton />
+    );
+  }
+}
+
+export default IconDemo;
