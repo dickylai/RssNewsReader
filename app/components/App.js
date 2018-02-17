043106b104cb5ea88be3a6e95ac2341138b86f41
makachung0 <makachung0@gmail.com>
2018-02-17 14:55:33 +0800

    Revert App.js
    Will be included in layout branch

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import NewsPanel from "./news/NewsPanel";
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>This is the content.</p>
-        <NewsPanel />
       </div>
     );
   }
