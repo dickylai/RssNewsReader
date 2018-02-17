4f9ded2e223690bf45c16229608d3c06d256b3e9
makachung0 <makachung0@gmail.com>
2018-02-17 20:56:36 +0800

    Layout basics

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import UpperContainer from './containers/UpperContainer';
+import LowerContainer from './containers/LowerContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>This is the content.</p>
+        <UpperContainer />
+        <LowerContainer />
       </div>
     );
   }
---
+++ app/components/containers/LowerContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FilterPanel from '../filters/FilterPanel';
+
+class LowerContainer extends Component {
+  render() {
+    return (
+      <div>
+        <FilterPanel />
+      </div>
+    );
+  }
+}
+
+export default LowerContainer;
